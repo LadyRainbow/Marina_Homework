b4f5d2e881673bad70f1b3c169e4a4e92bb429f3
LadyRainbow <m-trojchenko@gmail.com>
2016-11-14 01:52:31 +0200

    adaptiv

--- Catering/www/js/init.js
+++ Catering/www/js/init.js
@@ -1,9 +1,21 @@
-// init modal
-$('._order-modal-open').on('click', function(){
+;(function ($){
+	$(window).on('load', function(){
+		// init modal
+		$('._order-modal-open').on('click', function() {
 			$('#order').bPopup({
 				closeClass:'close',
+				onOpen: function(){
+					setTimeout(function(){
+						// formstyler
+						$('select').styler();
+					}, 50);
+				}
 			});
-		})
-
-// formstyler
-  // $('select').styler();
+		});
+		//slick nav
+		$('.menu-nav').slicknav({
+			prependTo: '.header .container',
+			label: '',
+		});
+	})
+})(jQuery)
